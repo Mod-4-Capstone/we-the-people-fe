347f9389b216eeadf09ecf212f82838412cd6156
Zach Liibbe <zliibbe@gmail.com>
2022-05-13 17:12:35 -0600

    add inital Quiz question 1

---
+++ src/components/Quiz/Quiz.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './Quiz.css'
+import Statement from "../Statement/statement"
+
+const Quiz = () => {
+
+  return (
+    <form className="quiz">
+      <Statement statement="Abortion should be legalized at the federal level"/>
+      
+    </form>
+  )
+} 
+
+export default Quiz
