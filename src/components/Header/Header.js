1c07d5813ef844f3976c49ae071762628eb5da9c
Zach Liibbe <zliibbe@gmail.com>
2022-05-13 17:11:07 -0600

    initialize functional Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './Header.css'
+
+const Header = () => {
+  return (
+    <section className="header">
+      <h1 className="title">We The People</h1>
+      {/* Take Quiz/ Home */}
+      {/* State Dropdown */}
+
+    </section>
+  )
+}
+
+export default Header
