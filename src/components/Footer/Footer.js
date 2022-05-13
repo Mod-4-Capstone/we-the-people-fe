9c0c0c0c6d7cab264224a30a89d2e71365c7a92b
Zach Liibbe <zliibbe@gmail.com>
2022-05-13 17:10:46 -0600

    initialize functional Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <section className="footer">
+      <p className="linked-in-links">LinkedIn Links Here</p>
+    </section>
+  )
+}
+
+export default Footer
