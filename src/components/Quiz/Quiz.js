4765f6cfaee7ca2a9ed5caebfc96eca95e69fd10
Zach Liibbe <zliibbe@gmail.com>
2022-05-13 17:33:54 -0600

    add 7 questions to quiz

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,13 +1,19 @@
 import React from "react"
 import './Quiz.css'
-import Statement from "../Statement/statement"
+import Statement from "../Statement/Statement"
 
 const Quiz = () => {
 
   return (
     <form className="quiz">
-      <Statement statement="Abortion should be legalized at the federal level"/>
-      
+      <Statement statement="Abortion should be legalized at the federal level."/>
+      <Statement statement="Public schools should be better funded."/>
+      <Statement statement="Healthcare should be universal."/>
+      <Statement statement="There should be more restrictions on the process of purchasing a gun."/>
+      <Statement statement="The U.S. government should increase environmental regulations in order to mitigate climate change."/>
+      <Statement statement="The U.S. government should raise the federal minimum wage."/>
+      <Statement statement="The U.S. government should build a wall along the southern border."/>
+      <Statement statement="The U.S. government should support a separation of church and state."/>
     </form>
   )
 } 
