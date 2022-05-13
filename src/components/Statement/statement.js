040eef8d29aa2dde06e3ba290b13ceaacd596129
Zach Liibbe <zliibbe@gmail.com>
2022-05-13 17:34:30 -0600

    change input values to numbers

--- src/components/Statement/statement.js
+++ src/components/Statement/statement.js
@@ -1,5 +1,5 @@
 import React from "react"
-import './statement.css'
+import './Statement.css'
 
 const Statement = (props) => {
   return (  
@@ -7,23 +7,23 @@ const Statement = (props) => {
     <label className="statement" htmlFor="likert-scale">{props.statement}</label>
     <ul className='likert'>
       <li>
-        <input type="radio" name="likert" value="strong_agree"/>
+        <input type="radio" name="likert" value="5"/>
         <label>Strongly agree</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="strong_agree"/>
+        <input type="radio" name="likert" value="4"/>
         <label>Agree</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="strong_agree"/>
+        <input type="radio" name="likert" value="3"/>
         <label>Neutral</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="disagree"/>
+        <input type="radio" name="likert" value="2"/>
         <label>Disagree</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="strong_agree"/>
+        <input type="radio" name="likert" value="1"/>
         <label>Strongly disagree</label>
       </li>
      </ul>
