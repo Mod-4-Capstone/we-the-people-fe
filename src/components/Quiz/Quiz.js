c9643d9198c4e403bb21c7de29823244132c0067
Zach Liibbe <zliibbe@gmail.com>
2022-05-14 10:44:47 -0600

    Add useState and form names for each question

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -17,7 +17,7 @@ const Quiz = () => {
   })
 
   const handleChange = (e) => {
-
+    setFormFields({...formFields, [e.target.name] : e.target.value })
   }
 
 
@@ -31,16 +31,17 @@ const Quiz = () => {
         <option value="51-61">51-61</option>
         <option value="62-70">62-70</option>
         <option value="70+">70+</option>
+        <option value="deferred">Prefer not to say</option>
       </select>
       <input type="text" placeholder="Enter your zipcode" name="zipcode" value="" onChange={(e) => handleChange(e)}></input>
-      <Statement statement="Abortion should be legalized at the federal level." id="1" setFormFields={setFormFields} handleChange={handleChange}/>
-      <Statement statement="Public schools should be better funded." id="2" setFormFields={setFormFields} handleChange={handleChange}/>
-      <Statement statement="Healthcare should be universal." id="3" setFormFields={setFormFields} handleChange={handleChange}/>
-      <Statement statement="There should be more restrictions on the process of purchasing a gun." id="4" setFormFields={setFormFields} handleChange={handleChange}/>
-      <Statement statement="The U.S. government should increase environmental regulations in order to mitigate climate change." id="5" setFormFields={setFormFields} handleChange={handleChange}/>
-      <Statement statement="The U.S. government should raise the federal minimum wage." id="6" setFormFields={setFormFields} handleChange={handleChange}/>
-      <Statement statement="The U.S. government should build a wall along the southern border." id="7" setFormFields={setFormFields} handleChange={handleChange}/>
-      <Statement statement="The U.S. government should support a separation of church and state." id="8" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="Abortion should be legalized at the federal level." name="Q1" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="Public schools should be better funded." name="Q2" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="Healthcare should be universal." name="Q3" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="There should be more restrictions on the process of purchasing a gun." name="Q4" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="The U.S. government should increase environmental regulations in order to mitigate climate change." name="Q5" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="The U.S. government should raise the federal minimum wage." name="Q6" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="The U.S. government should build a wall along the southern border."  name="Q7" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="The U.S. government should support a separation of church and state." name="Q8" setFormFields={setFormFields} handleChange={handleChange}/>
       <button type="submit">Submit</button>
     </form>
   )
