8de689fb2b0903482e252d270398f3633bc1c16c
Zach Liibbe <zliibbe@gmail.com>
2022-05-14 10:28:56 -0600

    Adjust values of likert scale

--- src/components/Statement/statement.js
+++ src/components/Statement/statement.js
@@ -5,25 +5,25 @@ const Statement = (props) => {
   return (  
   <>
     <label className="statement" htmlFor="likert-scale">{props.statement}</label>
-    <ul className='likert'>
+    <ul className='likert'  onChange={(e) => props.handleChange(e)}>
       <li>
-        <input type="radio" name="likert" value="5"/>
+        <input type="radio" name="likert" value="100"/>
         <label>Strongly agree</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="4"/>
+        <input type="radio" name="likert" value="75"/>
         <label>Agree</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="3"/>
+        <input type="radio" name="likert" value="50"/>
         <label>Neutral</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="2"/>
+        <input type="radio" name="likert" value="25"/>
         <label>Disagree</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="1"/>
+        <input type="radio" name="likert" value="0"/>
         <label>Strongly disagree</label>
       </li>
      </ul>
