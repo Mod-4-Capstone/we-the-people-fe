5202339a180a11d8b700a22b390495545847f03a
Zach Liibbe <zliibbe@gmail.com>
2022-05-14 10:29:39 -0600

    add onChange to form and disable inital dropdown

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -6,14 +6,25 @@ const Quiz = () => {
   const [formFields, setFormFields] = useState({
     age: "",
     zipcode: "",
-
+    Q1: "",
+    Q2: "",
+    Q3: "",
+    Q4: "",
+    Q5: "",
+    Q6: "",
+    Q7: "",
+    Q8: "",
   })
+
+  const handleChange = (e) => {
+
+  }
 
 
   return (
     <form className="quiz">
-      <select name="age">
-        <option value="" selected disabled>Select your Age Range</option>
+      <select defaultValue="default" name="age" onChange={(e) => handleChange(e)}>
+        <option value="default" disabled>Select your Age Range</option>
         <option value="18-28">18-28</option>
         <option value="29-39">29-39</option>
         <option value="40-50">40-50</option>
@@ -21,15 +32,15 @@ const Quiz = () => {
         <option value="62-70">62-70</option>
         <option value="70+">70+</option>
       </select>
-      <input type="text" placeholder="Enter your zipcode" name="zipcode" value=""></input>
-      <Statement statement="Abortion should be legalized at the federal level." id="1" setFormFields={setFormFields}/>
-      <Statement statement="Public schools should be better funded." id="2" setFormFields={setFormFields}/>
-      <Statement statement="Healthcare should be universal." id="3" setFormFields={setFormFields}/>
-      <Statement statement="There should be more restrictions on the process of purchasing a gun." id="4" setFormFields={setFormFields}/>
-      <Statement statement="The U.S. government should increase environmental regulations in order to mitigate climate change." id="5" setFormFields={setFormFields}/>
-      <Statement statement="The U.S. government should raise the federal minimum wage." id="6" setFormFields={setFormFields}/>
-      <Statement statement="The U.S. government should build a wall along the southern border." id="7" setFormFields={setFormFields}/>
-      <Statement statement="The U.S. government should support a separation of church and state." id="8" setFormFields={setFormFields}/>
+      <input type="text" placeholder="Enter your zipcode" name="zipcode" value="" onChange={(e) => handleChange(e)}></input>
+      <Statement statement="Abortion should be legalized at the federal level." id="1" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="Public schools should be better funded." id="2" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="Healthcare should be universal." id="3" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="There should be more restrictions on the process of purchasing a gun." id="4" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="The U.S. government should increase environmental regulations in order to mitigate climate change." id="5" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="The U.S. government should raise the federal minimum wage." id="6" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="The U.S. government should build a wall along the southern border." id="7" setFormFields={setFormFields} handleChange={handleChange}/>
+      <Statement statement="The U.S. government should support a separation of church and state." id="8" setFormFields={setFormFields} handleChange={handleChange}/>
       <button type="submit">Submit</button>
     </form>
   )
