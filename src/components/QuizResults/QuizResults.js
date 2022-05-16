e948238915c97220259be446791023a4bb0538cf
Zach Liibbe <zliibbe@gmail.com>
2022-05-16 10:13:04 -0600

    Initial component build for QuizResults

---
+++ src/components/QuizResults/QuizResults.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./QuizResults.css"
+
+const QuizResults = () => {
+  return ( 
+    <>
+      <div className="user-demo">
+        <h2>What's important to you:</h2>
+      </div>
+      <p>Abortion</p>
+      <p>Environmental</p>
+      <p>Other Stuff until it's all connected...</p>
+    </>
+   );
+}
+ 
+export default QuizResults;
