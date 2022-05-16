0555326e4f4fbc100e7526b3e349d69e640507b9
Madison Law <madisonlaw1@gmail.com>
2022-05-16 16:36:40 -0600

    Add section for state-wide stats

--- src/components/QuizResults/QuizResults.js
+++ src/components/QuizResults/QuizResults.js
@@ -3,14 +3,19 @@ import "./QuizResults.css"
 
 const QuizResults = () => {
   return ( 
-    <>
-      <div className="user-demo">
-        <h2>What's important to you:</h2>
-      </div>
-      <p>Abortion</p>
-      <p>Environmental</p>
-      <p>Other Stuff until it's all connected...</p>
-    </>
+    <section className="quiz-results-container">
+      <section className="user-quiz-results">
+        <div className="user-demo">
+          <h2>What's important to you:</h2>
+        </div>
+        <p>Abortion</p>
+        <p>Environmental</p>
+        <p>Other Stuff until it's all connected...</p>
+      </section>
+      <section className="state-wide-comparison">
+        <h2>Your beliefs match 40% with your state reps</h2>
+      </section>
+    </section>
    );
 }
  
