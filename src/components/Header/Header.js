c359984f584341c28ec132b12418ee11a8d48b16
Madison Law <madisonlaw1@gmail.com>
2022-05-16 16:25:40 -0600

    Add Link to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from "react"
 import './Header.css'
+import { Link } from "react-router-dom"
+
 
 const Header = () => {
   return (
     <section className="header">
-      <h1 className="title">We The People</h1>
+      <Link to='/'>  
+        <h1 className="title">We The People</h1>
+      </Link>
       {/* Take Quiz/ Home */}
       {/* State Dropdown */}
     </section>
