308a17551abbda317b1e23752828ee79940ee637
Jessica Fatta <jessicafatta@gmail.com>
2022-05-19 18:42:00 -0400

    Add classnames to details

--- src/components/RepCard/RepCard.js
+++ src/components/RepCard/RepCard.js
@@ -21,39 +21,39 @@ const RepCard = ({repData}) => {
         </div>
       </div>
       <div className="rep-issues-container">
-        <details>
+        <details className="details-arrow">
           <summary>Abortion: You match 40% on this issue </summary>
           <p className="rep-issues">Planned Parenthood: {repData.ratings.planned_parenthood}%</p>
         </details>
-        <details>
+        <details className="details-arrow">
           <summary> Budget, Spending: You match 40% on this issue </summary>
           <p className="rep-issues"> Americans for Prosperity: {repData.ratings.americans_for_prosperity}%</p>
         </details>
-        <details>
+        <details className="details-arrow">
           <summary>Civil Liberties You match 40% on this issue </summary>
           <p className="rep-issues"> ACLU: {repData.ratings.aclu}%</p>
         </details>
-        <details>
+        <details className="details-arrow">
           <summary>Campaign Finance:  You match 40% on this issue </summary>
           <p className="rep-issues"> End Citizens United: {repData.ratings.end_citizens_united}%</p>
         </details>
-        <details>
+        <details className="details-arrow">
           <summary>Criminal Justice:  You match 40% on this issue </summary>
           <p className="rep-issues"> Nat'l Assoc. of Police Org: {repData.ratings.national_assoc_of_police_org}%</p>
         </details>
-        <details>
+        <details className="details-arrow">
           <summary>Environment: You match 40% on this issue </summary>
           <p className="rep-issues"> Nat'l Parks Conservation Assoc.: {repData.ratings.national_parks_conservation_assoc}%</p>
         </details>
-        <details>
+        <details className="details-arrow">
           <summary>Guns: You match 40% on this issue </summary>
           <p className="rep-issues"> NRA: {repData.ratings.national_rifle_assoc}%</p>
         </details>
-        <details>
+        <details className="details-arrow">
           <summary>Immigration: You match 40% on this issue </summary>
           <p className="rep-issues"> NumbersUSA: {repData.ratings.numbers_usa}%</p>
         </details>
-        <details>
+        <details className="details-arrow">
           <summary>Marijuana: You match 40% on this issue </summary>
           <p className="rep-issues">  Nat'l Cannabis Industry Assoc: {repData.ratings.national_cannabis_industry_assoc}%</p>
         </details>
