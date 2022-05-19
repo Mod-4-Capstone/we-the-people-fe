4eec23774c07586b569c3d3eb4526220f753295f
Jessica Fatta <jessicafatta@gmail.com>
2022-05-19 19:23:17 -0400

    Add whats important to you statements to be dynamic later

--- src/components/QuizResults/QuizResults.js
+++ src/components/QuizResults/QuizResults.js
@@ -8,9 +8,16 @@ const QuizResults = () => {
         <div className="user-demo">
           <h2>What's important to you:</h2>
         </div>
-        <p>Abortion</p>
-        <p>Environmental</p>
-        <p>Other Stuff until it's all connected...</p>
+        <p>You are % in support of legalizing abortion federally.</p>
+        <p>You are % in support of better funding for public schools.</p>
+        <p>You are % in support of having less firearm purchase restrictions.</p>
+        <p>You are % in support of the federal government mitigating climate change.</p>
+        <p>You are % in support of the federal government raising minimum wage.</p>
+        <p>You are % in support of the federal government building a wall along the southern border.</p>
+        <p>You are % in support of transgender inclusion in sports.</p>
+        <p>You are % in support of the federal government legalizing marijuana.</p>
+        <p>You are % in support of funding the police.</p>
+        <p>You are % in support of campaign spending limits.</p>
       </section>
       <section className="state-wide-comparison">
         <h2>Your beliefs match 40% with your state reps</h2>
