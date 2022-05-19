5f9cf38a4f25c028c3cb1074431431c0bf31b7e1
Jessica Fatta <jessicafatta@gmail.com>
2022-05-19 18:31:54 -0400

    Update state to orgs and change name for target

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -7,14 +7,15 @@ const Quiz = (props) => {
   const [formFields, setFormFields] = useState({
     age: "",
     zipcode: "",
-    Q1: "",
-    Q2: "",
-    Q3: "",
-    Q4: "",
-    Q5: "",
-    Q6: "",
-    Q7: "",
-    Q8: "",
+    planned_parenthood: "",
+    national_education_association: "",
+    nra: "",
+    national_parks_conservation: "",
+    americans_for_prosperity: "",
+    numbers_usa: "",
+    aclu: "",
+    national_association_of_police: "",
+    norml: ""
   });
 
   const [isFormSubmitted, setIsFormSubmitted] = useState (false)
@@ -70,49 +71,55 @@ const Quiz = (props) => {
 
         <Statement
           statement="Abortion should be legalized at the federal level."
-          name="Q1"
+          name="planned_parenthood"
           setFormFields={setFormFields}
           handleChange={handleChange}
         />
         <Statement
           statement="Public schools should be better funded."
-          name="Q2"
-          setFormFields={setFormFields}
-          handleChange={handleChange}
-        />
-        <Statement
-          statement="Healthcare should be universal."
-          name="Q3"
+          name="national_education_association"
           setFormFields={setFormFields}
           handleChange={handleChange}
         />
         <Statement
           statement="There should be more restrictions on the process of purchasing a gun."
-          name="Q4"
+          name="nra"
           setFormFields={setFormFields}
           handleChange={handleChange}
         />
         <Statement
           statement="The U.S. government should increase environmental regulations in order to mitigate climate change."
-          name="Q5"
+          name="national_parks_conservation"
           setFormFields={setFormFields}
           handleChange={handleChange}
         />
         <Statement
           statement="The U.S. government should raise the federal minimum wage."
-          name="Q6"
+          name="americans_for_prosperity"
           setFormFields={setFormFields}
           handleChange={handleChange}
         />
         <Statement
           statement="The U.S. government should build a wall along the southern border."
-          name="Q7"
+          name="numbers_usa"
           setFormFields={setFormFields}
           handleChange={handleChange}
         />
         <Statement
           statement="The U.S. government should support a separation of church and state."
-          name="Q8"
+          name="aclu"
+          setFormFields={setFormFields}
+          handleChange={handleChange}
+        />
+        <Statement
+          statement="The police should not be defunded"
+          name="national_association_of_police"
+          setFormFields={setFormFields}
+          handleChange={handleChange}
+        />
+        <Statement
+          statement="Marijuana should be federally legalized"
+          name="norml"
           setFormFields={setFormFields}
           handleChange={handleChange}
         />
