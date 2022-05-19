cf9fc9d728646f4b627ea0560073486a51569ecf
Zach Liibbe <zliibbe@gmail.com>
2022-05-18 22:19:55 -0600

    Add conditionally rendered home button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,21 @@
 import React from "react"
 import './Header.css'
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import { FaFlagUsa } from 'react-icons/fa'
 
 
 const Header = () => {
+  const location = useLocation()
+
   return (
     <section className="header">
       <FaFlagUsa />
-      <Link to='/'>  
         <h1 className="title">We The People</h1>
-      </Link>
-      {/* Take Quiz/ Home */}
+      {location.pathname === "/results-dashboard" && 
+        <Link to="/">
+          <button className="home-btn">Home</button>
+        </Link>}
+
       {/* State Dropdown */}
     </section>
   )
