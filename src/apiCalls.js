d69f2f46ac4a12ecec56663ec6a326ee7a1cbd9b
Jessica Fatta <jessicafatta@gmail.com>
2022-05-20 16:31:51 -0400

    Add new endpoint in api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,7 @@
 const postRepsWithQuiz = (newQuizResult) => {
-  return fetch('https://we-the-people-be.herokuapp.com/api/v1/zipcode_with_quiz', {
+  return fetch('https://we-the-people-be.herokuapp.com/api/v2/zipcode_with_quiz', {
     method: 'POST',
-    body: JSON.stringify({
-      newQuizResult
-    }),
+    body: JSON.stringify(newQuizResult),
     headers: {
       "Content-Type": "application/json",
     },
