1c801439c1d2f37b834353fb056a86e1ba22f7c1
Zach Liibbe <zliibbe@gmail.com>
2022-05-20 15:35:08 -0600

    Update access to legislators array

--- src/components/RepContainer/RepContainer.js
+++ src/components/RepContainer/RepContainer.js
@@ -2,23 +2,18 @@ import React, { useContext } from "react"
 import RepCard from "../RepCard/RepCard";
 import "./RepContainer.css"
 import { DataContext } from '../../contexts/DataContext'
-// import { legislators } from "../../data/data";
 
 const RepContainer = () => {
   const legislators = useContext(DataContext)
-  console.log(legislators)
   const createRepCards = () => {
-    return legislators.map(legislator => {
+    return legislators.legislators.map(legislator => {
       return <RepCard repData={legislator}/>
     })
   }
 
-
   return (
     <section className="rep-container">
      {createRepCards()}
-
-
     </section>
    );
 }
