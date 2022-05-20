797e3ff6c1c19a5c08b54de6d4bae4bda53be5e9
Zach Liibbe <zliibbe@gmail.com>
2022-05-20 16:26:27 -0600

    Add summaryStats and currentQuizResult to context

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -6,9 +6,11 @@ const DataContext = createContext()
 const DataContextProvider = ({children}) => {
 
   const [legislators, setLegislators] = useState([])
+  const [summaryStats, setSummaryStats] = useState({})
+  const [currentQuizResult, setCurrentQuizResult] = useState({})
 
   return (
-    <DataContext.Provider value={{legislators, setLegislators}}>
+    <DataContext.Provider value={{legislators, setLegislators, summaryStats, setSummaryStats, currentQuizResult, setCurrentQuizResult}}>
       {legislators && children}
     </DataContext.Provider>
   )
