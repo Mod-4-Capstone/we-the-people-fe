0f3f11b668abf5ff4b08d2de9f88c6063e5798db
Zach Liibbe <zliibbe@gmail.com>
2022-05-20 15:34:07 -0600

    Update information to reflect API call

--- src/components/RepCard/RepCard.js
+++ src/components/RepCard/RepCard.js
@@ -8,54 +8,54 @@ const RepCard = ({repData}) => {
     <div className="rep-card">
       <div className="bio-info">
         <div>
-          <img src={repData.bio.picture} className="rep-img"/>
+          <img src={repData.attributes.biography.photo} className="rep-img"/>
         </div>
         <div className="bio-text-container">
-          <p className="bio-text">{repData.bio.name}</p>
-          <p className="bio-text">State: {repData.bio.state}</p>
-          <p className="bio-text">Age: {repData.bio.age}</p>
-          <p className="bio-text">Gender: {repData.bio.gender}</p>
-          <p className="bio-text">Years in office: {repData.bio.years_in_office}</p>
-          <p className="bio-text">Up for re-election in: {repData.bio.next_reelection_yr}</p>
+          <p className="bio-text">{repData.attributes.biography.name}</p>
+          <p className="bio-text">State: {repData.attributes.biography.district}</p>
+          <p className="bio-text">Age: {repData.attributes.biography.age}</p>
+          <p className="bio-text">Gender: {repData.attributes.biography.gender}</p>
+          <p className="bio-text">Years in office: {repData.attributes.biography.years_in_office}</p>
+          <p className="bio-text">Up for re-election in: {repData.attributes.biography.next_election}</p>
           <p className="rep-match">90% match with your beliefs</p>
         </div>
       </div>
       <div className="rep-issues-container">
         <details className="details-arrow">
           <summary>Abortion: You match 40% on this issue </summary>
-          <p className="rep-issues">Planned Parenthood: {repData.ratings.planned_parenthood}%</p>
+          <p className="rep-issues">Planned Parenthood: {repData.attributes.rating.planned_parenthood}%</p>
         </details>
         <details className="details-arrow">
           <summary> Budget, Spending: You match 40% on this issue </summary>
-          <p className="rep-issues"> Americans for Prosperity: {repData.ratings.americans_for_prosperity}%</p>
+          <p className="rep-issues"> Americans for Prosperity: {repData.attributes.rating.americans_for_prosperity}%</p>
         </details>
         <details className="details-arrow">
           <summary>Civil Liberties You match 40% on this issue </summary>
-          <p className="rep-issues"> ACLU: {repData.ratings.aclu}%</p>
+          <p className="rep-issues"> ACLU: {repData.attributes.rating.aclu}%</p>
         </details>
         <details className="details-arrow">
           <summary>Campaign Finance:  You match 40% on this issue </summary>
-          <p className="rep-issues"> End Citizens United: {repData.ratings.end_citizens_united}%</p>
+          <p className="rep-issues"> End Citizens United: {repData.attributes.rating.end_citizens_united}%</p>
         </details>
         <details className="details-arrow">
           <summary>Criminal Justice:  You match 40% on this issue </summary>
-          <p className="rep-issues"> Nat'l Assoc. of Police Org: {repData.ratings.national_assoc_of_police_org}%</p>
+          <p className="rep-issues"> Nat'l Assoc. of Police Org: {repData.attributes.rating.national_association_of_police}%</p>
         </details>
         <details className="details-arrow">
           <summary>Environment: You match 40% on this issue </summary>
-          <p className="rep-issues"> Nat'l Parks Conservation Assoc.: {repData.ratings.national_parks_conservation_assoc}%</p>
+          <p className="rep-issues"> Nat'l Parks Conservation Assoc.: {repData.attributes.rating.national_parks_conservation}%</p>
         </details>
         <details className="details-arrow">
           <summary>Guns: You match 40% on this issue </summary>
-          <p className="rep-issues"> NRA: {repData.ratings.national_rifle_assoc}%</p>
+          <p className="rep-issues"> NRA: {repData.attributes.rating.nra}%</p>
         </details>
         <details className="details-arrow">
           <summary>Immigration: You match 40% on this issue </summary>
-          <p className="rep-issues"> NumbersUSA: {repData.ratings.numbers_usa}%</p>
+          <p className="rep-issues"> NumbersUSA: {repData.attributes.rating.numbers_usa}%</p>
         </details>
         <details className="details-arrow">
           <summary>Marijuana: You match 40% on this issue </summary>
-          <p className="rep-issues">  Nat'l Cannabis Industry Assoc: {repData.ratings.national_cannabis_industry_assoc}%</p>
+          <p className="rep-issues">  Nat'l Cannabis Industry Assoc: {repData.attributes.rating.norml}%</p>
         </details>
       </div>
     </div>
