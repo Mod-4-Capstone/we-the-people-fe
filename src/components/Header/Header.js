753eba9d467a1513787e6ef1141e6d836e1e1204
Nicole Valentini <ncvalentini@gmail.com>
2022-05-21 14:42:27 -0700

    Add state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,20 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import './Header.css'
 import StateDropdown from "../StateDropdown/StateDropdown"
-import { Link, useLocation } from "react-router-dom"
+import { Link, Redirect, useLocation } from "react-router-dom"
 import { FaFlagUsa } from 'react-icons/fa'
 import { DataContext } from '../../contexts/DataContext'
 
 const Header = () => {
   const repData = useContext(DataContext)
   const location = useLocation()
+  const [selectedState, setSelectedState] = useState('')
+
+  const resetSelectedState = () => {
+    repData.setIsFormSubmitted(false);
+    return !repData.isFormSubmitted ? setSelectedState('default') : console.log('NOT WORKING');
+
+  }
 
   return (
     <section className="header">
@@ -16,11 +23,11 @@ const Header = () => {
         <h1 className="title">We The People</h1>
       </div>
 
-      <StateDropdown />
+      <StateDropdown setSelectedState={setSelectedState} selectedState={selectedState} />
 
         {location.pathname === "/results-dashboard" &&
           <Link to="/">
-            <button className="home-btn" onClick={() => repData.setIsFormSubmitted(false)}>Home</button>
+            <button className="home-btn" onClick={() => resetSelectedState()}>Home</button>
           </Link>}
     </section>
   )
