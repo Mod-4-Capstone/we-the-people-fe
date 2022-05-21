f9ea6b6f0833e2cbc5ea779657f3d4c6b2051fdb
Nicole Valentini <ncvalentini@gmail.com>
2022-05-21 13:32:30 -0700

    Add DataContext

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useContext } from "react"
 import './Header.css'
 import StateDropdown from "../StateDropdown/StateDropdown"
 import { Link, useLocation } from "react-router-dom"
 import { FaFlagUsa } from 'react-icons/fa'
+import { DataContext } from '../../contexts/DataContext'
 
 const Header = () => {
+  const repData = useContext(DataContext)
   const location = useLocation()
 
   return (
@@ -13,12 +15,12 @@ const Header = () => {
         <FaFlagUsa />
         <h1 className="title">We The People</h1>
       </div>
-      
+
       <StateDropdown />
-      
-        {location.pathname === "/results-dashboard" && 
+
+        {location.pathname === "/results-dashboard" &&
           <Link to="/">
-            <button className="home-btn">Home</button>
+            <button className="home-btn" onClick={() => repData.setIsFormSubmitted(false)}>Home</button>
           </Link>}
     </section>
   )
