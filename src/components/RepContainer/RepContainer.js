a938447c111a4b2954596f0109222a38ae836023
Madison Law <madisonlaw1@gmail.com>
2022-05-22 15:16:43 -0600

    Sort reps by compatibility

--- src/components/RepContainer/RepContainer.js
+++ src/components/RepContainer/RepContainer.js
@@ -5,10 +5,20 @@ import { DataContext } from '../../contexts/DataContext'
 
 const RepContainer = () => {
   const legislators = useContext(DataContext)
+
   const createRepCards = () => {
-    return legislators.legislators.map(legislator => {
-      return <RepCard key={legislator.id} repData={legislator}/>
-    })
+    if (legislators.legislators.every(rep => rep.attributes.compatibility)) {
+      const sortedLegislators = legislators.legislators.sort((a, b) => {
+        return b.attributes.compatibility.toFixed(1) - a.attributes.compatibility.toFixed(1)
+      })
+      return sortedLegislators.map(legislator => {
+        return <RepCard key={legislator.id} repData={legislator}/>
+      })
+    } else {
+      return legislators.legislators.map(legislator => {
+        return <RepCard key={legislator.id} repData={legislator}/>
+      })
+    }
   }
 
   return (
@@ -19,3 +29,17 @@ const RepContainer = () => {
 }
 
 export default RepContainer;
+
+
+
+/*
+
+TO FIX
+-> California reps have no districts, all are listed as Senators with their district as CA
+-> Identify whether they're a Senator or Rep on their cards
+-> Need condition in RepCard if repData.attributes.rating.planned_parenthood is null to show default data
+-> 
+
+
+
+*/ 
