dc02d157ae8c1a3e6115a70f66d5c3f44ca2a8ff
Nicole Valentini <ncvalentini@gmail.com>
2022-05-24 11:27:12 -0600

    Add test for quiz header section

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -13,17 +13,24 @@ describe('Landing Page', () => {
     .get('.title').contains('We The People')
   });
 
+  it.only('Should allow the user to see a quiz header that provides demographic facts for the US population vs. Congress', () => {
+    cy.get('.quiz-header-container').should('be.visible')
+    .get('.quote').contains('The Constitution provides for proportional representation in the U.S. House of Representatives and the seats in the House are apportioned based on state population according to the constitutionally mandated Census. — U.S. Constitution, Amendment XIV, section 2')
+    .get('.gender').contains('Female')
+    .get('.us-population-rep').should('be.visible')
+    .get('.congress-rep').should('be.visible')
+    .get('.age').contains('65 or older')
+  });
+
   it('Should be able to visit the page and see a quiz form, and a state select dropdown', () => {
   cy.get('.quiz')
     .should('be.visible')
-    // .get('.quiz-header').contains(`Here's why you should take our quiz...`)
     .get('.age-dropdown').should('be.visible')
     .get('.zipcode-input').should('be.visible')
     .get('.statement').should('have.length', 10)
     .get('.state-form-container').should('be.visible')
     .get('option').should('have.length', 59)
-    //additionally test for option values in each of the selects?
-});
+  });
 
 it('Should allow the user to select an age range, zipcode, and answers to questions', () => {
 });
