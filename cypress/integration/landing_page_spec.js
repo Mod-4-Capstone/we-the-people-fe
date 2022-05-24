69370c7b350190672738fe5207680b1ac0a6db11
Nicole Valentini <ncvalentini@gmail.com>
2022-05-24 11:06:11 -0600

    Add test for quiz visibility

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -12,4 +12,14 @@ describe('Landing Page', () => {
   cy.get('.header').should('be.visible')
     .get('.title').contains('We The People')
   });
+
+  it('Should be able to visit the page and see a quiz form', () => {
+  cy.get('.quiz')
+    .should('be.visible')
+    // .get('.quiz-header').contains(`Here's why you should take our quiz...`)
+    .get('.age-dropdown').should('be.visible')
+    .get('.zipcode-input').should('be.visible')
+    .get('.statement').should('have.length', 10)
+    //additionally test for option values in each of the selects?
+});
 })
