f20064364ade5de38099913f0ae90a59701b753f
Nicole Valentini <ncvalentini@gmail.com>
2022-05-24 11:14:28 -0600

    Add tests for quiz and dropdown visibility

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -13,13 +13,22 @@ describe('Landing Page', () => {
     .get('.title').contains('We The People')
   });
 
-  it('Should be able to visit the page and see a quiz form', () => {
+  it('Should be able to visit the page and see a quiz form, and a state select dropdown', () => {
   cy.get('.quiz')
     .should('be.visible')
     // .get('.quiz-header').contains(`Here's why you should take our quiz...`)
     .get('.age-dropdown').should('be.visible')
     .get('.zipcode-input').should('be.visible')
     .get('.statement').should('have.length', 10)
+    .get('.state-form-container').should('be.visible')
+    .get('option').should('have.length', 59)
     //additionally test for option values in each of the selects?
 });
-})
+
+it('Should allow the user to select an age range, zipcode, and answers to questions', () => {
+});
+
+it('Should allow the user to submit a fully filled out quiz, and be redirected to a new page with the results', () => {
+});
+
+});
