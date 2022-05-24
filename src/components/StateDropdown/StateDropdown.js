457a41e6a01d610ac22d275c4d4d7f715d10d6b5
Madison Law <madisonlaw1@gmail.com>
2022-05-24 10:54:33 -0600

    Refactor to iterate through state data

--- src/components/StateDropdown/StateDropdown.js
+++ src/components/StateDropdown/StateDropdown.js
@@ -3,12 +3,21 @@ import './StateDropdown.css'
 import { postReps } from '../../apiCalls';
 import { DataContext } from "../../contexts/DataContext";
 import { Redirect } from "react-router-dom"
+import states from '../../data/data';
 
 
 
 const StateDropdown = (props) => {
 
   const repData = useContext(DataContext)
+
+  const createStateOptions = () => {
+    return states.map(state => {
+      return (
+        <option value={state.abbrev}>{state.fullName}</option>
+      )
+    })
+  }
 
   const packageEmptyQuiz = () => {
     const emptyQuiz = {
@@ -77,56 +86,7 @@ const StateDropdown = (props) => {
         <form className='state-form-container' onSubmit ={(e) => handleSubmit(e)}>
           <select name="states" className="state-dropdown" id="states" defaultValue='default' onChange={(e) => props.setSelectedState(e.target.value)}>
             <option value ='default' disabled>select state</option>
-            <option value="AL">Alabama</option>
-            <option value="AK">Alaska</option>
-            <option value="AZ">Arizona</option>
-            <option value="AR">Arkansas</option>
-            <option value="CA">California</option>
-            <option value="CO">Colorado</option>
-            <option value="CT">Connecticut</option>
-            <option value="DE">Delaware</option>
-            <option value="FL">Florida</option>
-            <option value="GA">Georgia</option>
-            <option value="HI">Hawaii</option>
-            <option value="ID">Idaho</option>
-            <option value="IL">Illinois</option>
-            <option value="IN">Indiana</option>
-            <option value="IA">Iowa</option>
-            <option value="KS">Kansas</option>
-            <option value="KY">Kentucky</option>
-            <option value="LA">Louisiana</option>
-            <option value="ME">Maine</option>
-            <option value="MD">Maryland</option>
-            <option value="MA">Massachusetts</option>
-            <option value="MI">Michigan</option>
-            <option value="MN">Minnesota</option>
-            <option value="MS">Mississippi</option>
-            <option value="MO">Missouri</option>
-            <option value="MT">Montana</option>
-            <option value="NE">Nebraska</option>
-            <option value="NV">Nevada</option>
-            <option value="NH">New Hampshire</option>
-            <option value="NJ">New Jersey</option>
-            <option value="NM">New Mexico</option>
-            <option value="NY">New York</option>
-            <option value="NC">North Carolina</option>
-            <option value="ND">North Dakota</option>
-            <option value="OH">Ohio</option>
-            <option value="OK">Oklahoma</option>
-            <option value="OR">Oregon</option>
-            <option value="PA">Pennsylvania</option>
-            <option value="RI">Rhode Island</option>
-            <option value="SC">South Carolina</option>
-            <option value="SD">South Dakota</option>
-            <option value="TN">Tennessee</option>
-            <option value="TX">Texas</option>
-            <option value="UT">Utah</option>
-            <option value="VT">Vermont</option>
-            <option value="VA">Virginia</option>
-            <option value="WA">Washington</option>
-            <option value="WV">West Virginia</option>
-            <option value="WI">Wisconsin</option>
-            <option value="WY">Wyoming</option>
+            {createStateOptions()}
           </select>
             <button className='go-button'>Go!</button>
             {repData.isFormSubmitted && <Redirect to="/results-dashboard"/>}
