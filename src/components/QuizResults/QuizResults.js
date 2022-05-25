7c0e92eee58f33c06149899fb99d75dd60b86100
Madison Law <madisonlaw1@gmail.com>
2022-05-24 20:57:57 -0600

    Put quiz results in tiles

--- src/components/QuizResults/QuizResults.js
+++ src/components/QuizResults/QuizResults.js
@@ -1,6 +1,7 @@
 import React, { useContext }from "react"
 import "./QuizResults.css"
 import { DataContext } from '../../contexts/DataContext'
+import QuizResultsModal from "../QuizResultsModal/QuizResultsModal"
 
 const QuizResults = () => {
 
@@ -23,19 +24,31 @@ const QuizResults = () => {
         <div className="user-demo">
           <h2>What's important to you:</h2>
         </div>
-        <p>You are {repData.currentQuizResult.planned_parenthood}% in support of legalizing abortion federally.</p>
-        <p>You are {repData.currentQuizResult.national_education_association}% in support of better funding for public schools.</p>
-        <p>You are {repData.currentQuizResult.nra}% in support of having less firearm purchase restrictions.</p>
-        <p>You are {repData.currentQuizResult.national_parks_conservation}% in support of the federal government mitigating climate change.</p>
-        <p>You are {repData.currentQuizResult.americans_for_prosperity}% in support of the federal government not raising minimum wage.</p>
-        <p>You are {repData.currentQuizResult.numbers_usa}% in support of the federal government building a wall along the southern border.</p>
-        <p>You are {repData.currentQuizResult.aclu}% in support of transgender inclusion in sports.</p>
-        <p>You are {repData.currentQuizResult.norml}% in support of the federal government legalizing marijuana.</p>
-        <p>You are {repData.currentQuizResult.national_association_of_police}% in support of funding the police.</p>
-        <p>You are {repData.currentQuizResult.end_citizens_united}% in support of campaign spending limits.</p>
+        <section className="result-tile-container">
+          <div className="result-tile">
+            <p className="percentage">{repData.currentQuizResult.planned_parenthood}%</p>
+            <p>in support of legalizing abortion federally</p>
+          </div>
+          <div className="result-tile">
+            <p className="percentage">{repData.currentQuizResult.national_education_association}%</p>
+            <p>in support of better funding for public schools.</p>
+          </div>
+          <div className="result-tile">
+            <p className="percentage">{repData.currentQuizResult.nra}%</p>
+            <p>in support of having less firearm purchase restrictions.</p>
+          </div>
+        </section>
+        <QuizResultsModal />
       </section>
       <section className="state-wide-comparison">
-        {repData.summaryStats.data && <h2>Your beliefs match {getStateAverageStats()}% with {repData.legislators[0].attributes.biography.state} representatives</h2>}
+        {repData.summaryStats.data && 
+        <>
+        <p className="state-percentage-text">Your beliefs match</p>
+        <p className="state-percentage">{getStateAverageStats()}%</p> 
+        <p className="state-percentage-text">with {repData.legislators[0].attributes.biography.state} legislators</p>
+        <p className="state-percentage-text">overall</p>
+        </>
+        }
       </section>
     </section>
    );
