c3b3e1744b1189441db230f2dacd98f57dda66b5
Nicole Valentini <ncvalentini@gmail.com>
2022-05-25 13:27:14 -0600

    Add class names to bio

--- src/components/RepCard/RepCard.js
+++ src/components/RepCard/RepCard.js
@@ -23,19 +23,19 @@ const RepCard = ({repData}) => {
         </div>
         <div className="bio-text-container">
           <p className="bio-text rep-name">{repData.attributes.biography.congressional_type} {repData.attributes.biography.name}</p>
-          <p className="bio-text">Party: {repData.attributes.biography.party}</p>
+          <p className="bio-text bio-party">Party: {repData.attributes.biography.party}</p>
           {repData.attributes.biography.congressional_type === 'Representative' ?  <p className="bio-text">District: {repData.attributes.biography.district}</p> :  <p className="bio-text">State: {repData.attributes.biography.district}</p>}
-          <p className="bio-text">Age: {repData.attributes.biography.age}</p>
-          <p className="bio-text">Gender: {repData.attributes.biography.gender}</p>
-          <p className="bio-text">Years in office: {repData.attributes.biography.years_in_office}</p>
-            {data.currentQuizResult.nra && 
+          <p className="bio-text bio-age">Age: {repData.attributes.biography.age}</p>
+          <p className="bio-text bio-gender">Gender: {repData.attributes.biography.gender}</p>
+          <p className="bio-text bio-term">Years in office: {repData.attributes.biography.years_in_office}</p>
+            {data.currentQuizResult.nra &&
               (repData.attributes.compatibility ? <p className="rep-match">{(repData.attributes.compatibility).toFixed(1)}% match with your beliefs</p> : <p className="rep-match">Compatibility unavailable</p>)
             }
-            
+
         </div>
       </div>
       <div className="rep-contact-container">
-        {repData.attributes.representative_social.contact_page && 
+        {repData.attributes.representative_social.contact_page &&
         <>
           <FiMail className="mail-icon"/>
           <a className="contact-text" href={repData.attributes.representative_social.contact_page}>Contact {repData.attributes.biography.congressional_type} {(repData.attributes.biography.name).split(' ').slice(-1).join(' ')} here</a>
