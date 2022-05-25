06c8b1ec7020e0c71d60eee2094302f166260dc3
Nicole Valentini <ncvalentini@gmail.com>
2022-05-25 14:53:43 -0600

    Add test for contact link

--- cypress/integration/state_only_spec.js
+++ cypress/integration/state_only_spec.js
@@ -16,4 +16,23 @@ describe('State dropdown without a quiz flow', () => {
       .url().should('include', 'results-dashboard')
       .get('.rep-card').should('have.length', 6)
   });
+
+  it('Should show each legislators bio information, and ratings', () => {
+    cy.get('.state-dropdown').select('Iowa')
+      .get('.go-button').click()
+      .get('.rep-name').first().contains('Representative Cynthia Axne')
+      .get('.bio-party').first().contains('Party: Democrat')
+      .get('.bio-age').first().contains('Age: 57')
+      .get('.bio-gender').first().contains('Gender: Female')
+      .get('.bio-term').first().contains('Years in office: 4')
+      .get('.abortion-text').first().contains('Planned Parenthood rates this legislator at 100% on Abortion')
+      .get('.mj-text').first().contains('Norml rates this legislator at 42% on Marijuana')
+      .get('.contact-text').first().contains('Contact Representative Axne here')
+    });
+
+    it('Should allow the user to click a legislators contact info, and be redirected to their personal contact page', () => {
+      cy.get('.state-dropdown').select('Iowa')
+        .get('.go-button').click()
+        .url().should('include', 'results-dashboard')
+    });
 });
